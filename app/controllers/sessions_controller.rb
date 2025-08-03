4fe9359e0bdf7ad5a360181a02b1c3a31ce702e5
Paul Seica <pseica01@DESKTOP-C837STU.localdomain>
2025-08-03 15:27:24 +0300

    Rubocop formatted

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
-  
+
   def create
     if user = User.authenticate_by(user_params)
       start_new_session_for user
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
     redirect_to new_session_path
   end
 
-  private 
+  private
 
   def user_params
     params.expect(user: [ :email_address, :password ])
