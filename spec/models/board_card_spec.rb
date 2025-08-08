0925edb852cc5328c6ea483943d8fd7d836a4ea5
Paul Seica <pseica01@DESKTOP-C837STU.localdomain>
2025-08-08 17:18:37 +0300

    Made BoardCard tests

--- spec/models/board_card_spec.rb
+++ spec/models/board_card_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe BoardCard, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { User.create!(username: "username", email_address: "email@email.com", password: "password123") }
+  let!(:deck) { Deck.create!(power_level: 1, user_id: user.id) }
+  let!(:board_type) { BoardType.create!(name: "Board Type") }
+  let!(:board) { Board.create!(extra_deck_summons: 3, hand_summons: 3, gy_banishment_summons: 3, deck: deck, board_type: board_type)}
+  let!(:card){ MonsterCard.create(name: "Card", description: "Desc", card_type: "dragon") }
+
+  context "when the position" do
+    context "is not within the allowed values" do
+      it "is invalid" do
+        valid_zones = %w[ mmz1 mmz2 mmz3 mmz4 mmz5 stz1 stz2 stz3 stz4 stz5 emz1 emz2 field graveyard banishment deck extra_deck ]
+        valid_zones.each do |zone|
+          board_card = BoardCard.new(board: board, card: card, position: zone)
+          expect(board_card).to be_valid
+        end
+      end
+    end
+  end
 end
