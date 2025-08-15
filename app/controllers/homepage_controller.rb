204b971c2c91de3c5e144990e773d2cd13f51cb4
Paul Seica <pseica01@DESKTOP-C837STU.localdomain>
2025-08-15 20:33:56 +0300

    Made first section of homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,5 +2,14 @@ class HomepageController < ApplicationController
   allow_unauthenticated_access
   def index
     @user = authenticated? ? find_session_by_cookie.user : User.new
+    @tiers = {
+      "any": "Random Deck",
+      "1": 'Top Tier Decks',
+      "2": 'Competitive Decks',
+      "3": 'Competitive Potential Decks',
+      "4": 'Rogue Decks',
+      "5": 'Weak Decks'
+    }
+    @archetypes = Archetype.all
   end
 end
