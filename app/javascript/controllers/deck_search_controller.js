a915037c9185aeb26e09760cc9165d9bad032be4
Paul Seica <seicapaul11@gmail.com>
2025-08-25 18:40:19 +0300

    Made searching through user decks

--- app/javascript/controllers/deck_search_controller.js
+++ app/javascript/controllers/deck_search_controller.js
@@ -3,18 +3,25 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     window.decks_table = this.decks_table;
+    console.log(decks_table);
   }
   
   search_decks(){
     let query = this.query
-
+    
     for(let i = 0; i < decks_table.children.length; i++){
       let deck = decks_table.children[i];
-
-      if(!deck.innerHTML.toLowerCase().includes(query))
-        deck.style.display = "none";
-      else
+      let search_target = deck;
+      if(deck.firstChild)
+        search_target = deck.querySelector(".search_target");
+      
+      console.log(search_target.innerHTML.toLowerCase().includes(query.toLowerCase()));
+      console.log(search_target.innerHTML.toLowerCase());
+      console.log(query.toLowerCase()); 
+      if(search_target.innerHTML.toLowerCase().includes(query.toLowerCase()))
         deck.style.display = "inline-block";
+      else
+        deck.style.display = "none";
     }
   }
 
