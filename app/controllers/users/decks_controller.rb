26bf1e518c5ed686cc9994464fb3be92af5152b3
Paul Seica <seicapaul11@gmail.com>
2025-09-10 11:25:05 +0300

    Started the view deck page

--- app/controllers/users/decks_controller.rb
+++ app/controllers/users/decks_controller.rb
@@ -1,21 +1,29 @@
 class Users::DecksController < ApplicationController
   before_action :get_user
+  before_action :get_deck, only: [ :show ]
 
   def new
     @deck = @user.decks.build
   end
 
   def show
-
+    set_deck_title
   end
 
   private
 
   def get_deck
-    @deck = Deck.find(params[:deck_id])
+    @deck = @user.decks.find(params[:id])
   end
 
   def get_user
     @user = User.find(params[:user_id])
+  end
+
+  def set_deck_title
+    @title = ""
+    @deck.archetypes.each do |archetype|
+      @title = "#{@title} #{archetype.name}"
+    end
   end
 end
