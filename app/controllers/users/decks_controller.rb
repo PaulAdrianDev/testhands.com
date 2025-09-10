3987fb73c58693eedd17093a5d81aac728f420a6
Paul Seica <seicapaul11@gmail.com>
2025-09-10 10:34:10 +0300

    Made empty show for decks

--- app/controllers/users/decks_controller.rb
+++ app/controllers/users/decks_controller.rb
@@ -1,13 +1,19 @@
 class Users::DecksController < ApplicationController
   before_action :get_user
-  def index
-  end
 
   def new
     @deck = @user.decks.build
   end
 
+  def show
+
+  end
+
   private
+
+  def get_deck
+    @deck = Deck.find(params[:deck_id])
+  end
 
   def get_user
     @user = User.find(params[:user_id])
