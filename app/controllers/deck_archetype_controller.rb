f0da51ef3e7d031f965321ae8ec73d7ae0d8a4c6
Paul Seica <seicapaul11@gmail.com>
2025-09-16 16:48:28 +0300

    Made adding archetypes to deck

--- app/controllers/deck_archetype_controller.rb
+++ app/controllers/deck_archetype_controller.rb
@@ -1,6 +1,15 @@
 class DeckArchetypeController < ApplicationController
   def create
-  
+    deck_id = params.expect(:deck_id)
+    archetype_id = params.expect(:archetype_id)
+
+    deck_archetype = DeckArchetype.new(deck_id: deck_id, archetype_id: archetype_id)
+
+    if deck_archetype.save
+      redirect_back fallback_location: root_path, notice: "Archetype Added Successfully."
+    else
+      redirect_back fallback_location: root_path, alert: "Error Adding Archetype."
+    end
   end
 
   def destroy
