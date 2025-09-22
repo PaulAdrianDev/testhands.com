d2e00c1be98d6032e70f0c5a3fec8ea5cbf94c1a
Paul Seica <seicapaul11@gmail.com>
2025-09-22 12:45:27 +0300

    Made archetypes table close

--- app/javascript/controllers/editing_deck_controller.js
+++ app/javascript/controllers/editing_deck_controller.js
@@ -1,8 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  openArchetypesTable(){
-    this.archetypes_table.style.maxHeight = "400px";
+  openArchetypesTable(event){
+    let table = this.archetypes_table;
+    if(table.style.maxHeight == "400px"){
+      table.style.maxHeight = "0px";
+      event.target.textContent = "+ Add Archetype";
+    }
+    else{
+      table.style.maxHeight = "400px";
+      event.target.textContent = "X Close Archetype Table";
+    }
   }
 
   get archetypes_table(){
