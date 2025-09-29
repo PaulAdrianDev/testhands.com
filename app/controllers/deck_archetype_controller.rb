63d4a3e3a1017a18ff9863bd3d8bc9bf3824da68
Paul Seica <seicapaul11@gmail.com>
2025-09-29 11:40:22 +0300

    Tidied up DeckArchetype controller

--- app/controllers/deck_archetype_controller.rb
+++ app/controllers/deck_archetype_controller.rb
@@ -1,24 +1,27 @@
 class DeckArchetypeController < ApplicationController
   def create
-    deck_id = params.expect(:deck_id)
-    archetype_id = params.expect(:archetype_id)
+    deck = Deck.find(params.expect(:deck_id))
+    archetype = Archetype.find(params.expect(:archetype_id))
+    user = deck.user
 
-    deck_archetype = DeckArchetype.new(deck_id: deck_id, archetype_id: archetype_id)
+    deck_archetype = DeckArchetype.new(deck: deck, archetype: archetype)
 
     if deck_archetype.save
-      redirect_back fallback_location: root_path, notice: "Archetype Added Successfully."
+      redirect_to edit_user_deck_path(user, deck), notice: "Archetype Added Successfully."
     else
-      redirect_back fallback_location: root_path, alert: "Error Adding Archetype."
+      redirect_to edit_user_deck_path(user, deck), alert: "Error Adding Archetype."
     end
   end
 
   def destroy
     deck_archetype = DeckArchetype.find(params.expect(:id))
+    deck = deck_archetype.deck
+    user = deck.user
 
     if deck_archetype.destroy
-      redirect_back fallback_location: root_path, notice: "Archetype Removed Successfully."
+      redirect_to edit_user_deck_path(user, deck), notice: "Archetype Removed Successfully."
     else
-      redirect_back fallback_location: root_path, alert: "Error Removing Archetype."
+      redirect_to edit_user_deck_path(user, deck), alert: "Error Removing Archetype."
     end
   end
 end
