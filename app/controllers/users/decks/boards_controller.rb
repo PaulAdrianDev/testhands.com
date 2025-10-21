66017621985b670a5178a16078c23fecf20d2c6e
Paul Seica <pseica01@DESKTOP-C837STU.localdomain>
2025-10-21 14:52:51 +0300

    Made adding a new board and taking you to edit it

--- app/controllers/users/decks/boards_controller.rb
+++ app/controllers/users/decks/boards_controller.rb
@@ -6,6 +6,22 @@ class Users::Decks::BoardsController < ApplicationController
     @board = Board.new
   end
 
+  def create
+    type = BoardType.find_by(name: params[:board_type])
+
+    if type.blank?
+      return redirect_to edit_user_deck_path(@user, @deck), alert: "An error has occured, please report this to us. INVALID_BOARD_TYPE"
+    end
+
+    board = Board.new(deck_summons: 0, hand_summons: 0, gy_banishment_summons: 0, information: "", deck: @deck, board_type: type)
+
+    if board.save
+      redirect_to edit_user_deck_board_path(@user, @deck, board), notice: "Board Added Successfuly."
+    else
+      redirect_to edit_user_deck_path(@user, @deck), alert: "An error has occured, please report this to us. ERROR_CREATING_BOARD"
+    end
+  end
+
   private
   
   def set_user
