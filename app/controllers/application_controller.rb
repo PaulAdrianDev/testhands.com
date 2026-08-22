e5a9e21cdc330f8dbcb4abdfc38f6eb4f9a93fd3
Paul Seica <seicapaul11@gmail.com>
2026-08-22 21:21:17 +0300

    Hid Delete and Edit buttons when viewing other user's profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :set_logged_in_user
+
+  def set_logged_in_user
+    if authenticated?
+      @logged_in_user = User.find(Session.find_by(id: cookies.signed[:session_id])[:user_id])
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-
   def any_information_is_missing?
     params_needed = [ :username, :email_address, :password, :password_confirmation ]
 
